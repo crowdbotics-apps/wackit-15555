7ce5075dd443b37d217a5f47164363823023dfc4
Crowdbotics <team@crowdbotics.com>
2020-04-07 18:01:20 +0000

    Initial commit.

---
+++ backend/wackit_15555/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wackit_15555 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wackit_15555.settings')
+
+application = get_wsgi_application()
